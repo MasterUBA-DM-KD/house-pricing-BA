8fea40d963dddee30ec12e74ecccde2d6e408b9c
Alejandro Uribe <jauriberamirez@gmail.com>
2023-06-05 00:19:10 -0300

    WIP: impute barrios

--- src/constants.py
+++ src/constants.py
@@ -3,10 +3,10 @@ DATA_TEST_PATH = "data/properati_a_predecir.parquet"
 
 
 RENAME_COLS = {
-"l1": "country",
-"l2": "province",
-"l3": "city_neighorhood",
-"l4": "published_neigborhood",
+    "l1": "country",
+    "l2": "province",
+    "l3": "city_neighorhood",
+    "l4": "published_neigborhood",
 }
 
 PROVINCE = [
@@ -18,6 +18,10 @@ PROVINCE = [
 
 PROPERTY_TYPE = ["PH", "Departamento"]
 COUNTRY = ["Argentina"]
-DROP_COLS = ["l5","l6"]
+DROP_COLS = ["l5", "l6"]
 CURRENCY = ["USD"]
 OPERATION_TYPE = ["Venta"]
+
+
+centro_geografico_caba = [-34.61448064637565, -58.44638197269814]
+centro_geografico_plata = [-34.92137784728068, -57.95455274162061]
